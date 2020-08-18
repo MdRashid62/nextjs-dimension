ff2d684c95b98365d977ee025aecaf729ee8dca9
Muhammad Rashid <55427374+MdRashid62@users.noreply.github.com>
2020-08-18 16:42:53 +0500

    commit changes

--- components/Main.js
+++ components/Main.js
@@ -63,7 +63,7 @@ class Main extends React.Component {
             <li><a href="https://facebook.com/">
               <FontAwesomeIcon icon={faFacebook} />
             </a></li>
-            <li><a href="#">
+            <li><a href="https://instagram.com/">
               <FontAwesomeIcon icon={faInstagram} />
             </a></li>
             <li><a href="#">
